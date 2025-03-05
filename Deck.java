94f9931c2e70c51cef7d50dd9fa0f34142f52323
Aastha Chordia <aasthachordia595@gmail.com>
2025-03-05 23:29:00 +0530

    create the deck add all the cards

--- Deck.java
+++ Deck.java
@@ -4,5 +4,10 @@ class Deck{
 		public Deck(){
 				this.deck = new ArrayList<>();
 		}
+		public void createDeck(){
+				String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9","10", "J", "Q", "K", "A"};
+				String[] suits = {"Clubs", "Spades", "Hearts", "Diamond"};
+				
+				
 		
 		
